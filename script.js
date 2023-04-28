733ca4890c76a803106a910920b7dd66e3b58b70
Tiago Rodrigues <tiago_rodriguessouza@hotmail.com>
2023-04-28 13:29:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const clouds = document.querySelector('.clouds');
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
+const go = document.getElementById("go");
+const rest = document.getElementById("restart");
 
-const jump =() =>{
-   mario.classList.add('jump');
+const jump = () => {
+  mario.classList.add("jump");
 
-   setTimeout(()=>{
-    mario.classList.remove('jump');
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 1000);
+};
 
-   },1000 );
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = window.getComputedStyle(mario).bottom.replace("px", "");
+  const cloudsPosition = window.getComputedStyle(clouds).left.replace("px", "");
+  const goPosition = window.getComputedStyle(clouds).left.replace("px", "");
 
-}
+  const resPosition = window.getComputedStyle(clouds).left.replace("px", "");
 
+  if (pipePosition <= 95 && pipePosition > 0 && marioPosition < 90) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-const loop =setInterval(()=>{
+    clouds.style.animation = "none";
+    clouds.style.left = `${cloudsPosition}px`;
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
-    const cloudsPosition = window.getComputedStyle(clouds).left.replace('px', '');
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
 
-    if ( pipePosition <= 95 && pipePosition > 0 && marioPosition < 90  ){
-        
-        pipe.style.animation = 'none';    
-        pipe.style.left = `${pipePosition}px`;
+    go.style.animation = "none";
+    go.style.left = `${goPosition}px`;
 
-        clouds.style.animation = 'none';
-        clouds.style.left = `${cloudsPosition}px`;
-    
+    mario.src =
+      "https://cdn.streamelements.com/uploads/3a66481b-e161-40ce-8e3c-8bb0f42145b4.png";
+    mario.style.width = "125px";
+    mario.style.height = "135px";
+    mario.style.marginLeft = "30px";
+    go.innerText = "GAME OVER";
+    rest.style.opacity = "1";
 
-        mario.style.animation = 'none';    
-        mario.style.bottom = `${marioPosition}px`;
+    clearInterval(loop);
+  }
+}, 10);
 
-        mario.src='./images/game-over.png';
-        mario.style.width ='75px'
-        mario.style.marginLeft ='50px'
-
-
-        clearInterval(loop);
-        
-    }
-   
-},10);
-
-document.addEventListener('keydown', jump);
-document.addEventListener('touchstart', jump);
-document.addEventListener('click', jump);
+document.addEventListener("keydown", jump);
+document.addEventListener("touchstart", jump);
+document.addEventListener("click", jump);
